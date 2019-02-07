11eabaff465748ff31d48f9c2887f0c7e51dd13a
AndrewJeannett <andrewjeannett@gmail.com>
2019-02-07 17:57:48 -0600

    Initial commit

---
+++ Assets/Grid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+private GameObject [,] grid;
+public int GridNum;
+public GameObject gridtile;
+
+
+	// Use this for initialization
+	void Start () {
+		grid = new GameObject [GridNum,GridNum];
+		for (int p =0; p<GridNum; p++){
+			for (int i = 0; i<GridNum;i++){
+				Vector3 tilePos = new Vector3 (-GridNum/2 +p*2,0,-GridNum/2 +i*2);
+				  GameObject tile = Instantiate(gridtile, tilePos, Quaternion.Euler(90, 0, 0));
+				  grid[p, i] = tile; 
+
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/PiecePlacement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiecePlacement : MonoBehaviour {
+public GameObject RampPiece1;
+public GameObject SelectionBox;
+private int Rotation;
+    private bool rotateRefresh;
+	// Use this for initialization
+	void Start () {
+		Rotation=90;
+	}
+	public void Update(){
+        if (Input.GetButtonDown("Jump"))
+        {
+            Rotation += 90;
+            Debug.Log("rotate");
+            rotateRefresh = true;
+            foreach (Transform child in transform)
+            {
+                float i = 0;
+                i += 1;
+                Destroy(child.gameObject);
+
+            }
+        }
+	}
+     void OnMouseDown()
+    {
+        Vector3 piecePos= new Vector3(transform.position.x, transform.position.y + .1f, transform.position.z);
+		Instantiate(RampPiece1, piecePos,Quaternion.Euler(0,Rotation+90,0));
+	}
+	void OnMouseEnter(){
+		Vector3 piecePos= new Vector3(transform.position.x, transform.position.y + .1f, transform.position.z);
+        if (rotateRefresh == true)
+        {
+            Instantiate(SelectionBox, piecePos, Quaternion.Euler(90, Rotation, 0), transform);
+            rotateRefresh = false;
+        }
+	}
+    void OnMouseExit(){
+
+        foreach (Transform child in transform) {
+	    float i = 0;
+        i += 1;
+        Destroy(child.gameObject);
+
+		}
+    }
+}
