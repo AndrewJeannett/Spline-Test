18dcfaa87b6f57a726ac38f6c597bff5636dd600
AndrewJeannett <andrewjeannett@gmail.com>
2019-02-07 20:54:44 -0600

    roattion

--- Assets/PiecePlacement.cs
+++ Assets/PiecePlacement.cs
@@ -2,50 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PiecePlacement : MonoBehaviour {
-public GameObject RampPiece1;
-public GameObject SelectionBox;
-private int Rotation;
-    private bool rotateRefresh;
-	// Use this for initialization
-	void Start () {
-		Rotation=90;
-	}
-	public void Update(){
+public class PiecePlacement : MonoBehaviour
+{
+    public GameObject RampPiece1;
+    public GameObject SelectionBox;
+    private int Rotation;
+
+    // Use this for initialization
+    void Start()
+    {
+        Rotation = 90;
+    }
+    public void Update()
+    {
         if (Input.GetButtonDown("Jump"))
         {
             Rotation += 90;
             Debug.Log("rotate");
-            rotateRefresh = true;
-            foreach (Transform child in transform)
-            {
-                float i = 0;
-                i += 1;
-                Destroy(child.gameObject);
-
-            }
+            transform.GetChild(0).rotation = Quaternion.Euler(0, Rotation+90, 0);
         }
-	}
-     void OnMouseDown()
+    }
+    void OnMouseDown()
     {
-        Vector3 piecePos= new Vector3(transform.position.x, transform.position.y + .1f, transform.position.z);
-		Instantiate(RampPiece1, piecePos,Quaternion.Euler(0,Rotation+90,0));
-	}
-	void OnMouseEnter(){
-		Vector3 piecePos= new Vector3(transform.position.x, transform.position.y + .1f, transform.position.z);
-        if (rotateRefresh == true)
-        {
-            Instantiate(SelectionBox, piecePos, Quaternion.Euler(90, Rotation, 0), transform);
-            rotateRefresh = false;
-        }
-	}
-    void OnMouseExit(){
+        Vector3 piecePos = new Vector3(transform.position.x, transform.position.y + .1f, transform.position.z);
+        Instantiate(RampPiece1, piecePos, Quaternion.Euler(0, Rotation + 90, 0));
+    }
+    void OnMouseEnter()
+    {
+        Vector3 piecePos = new Vector3(transform.position.x, transform.position.y + .1f, transform.position.z);
 
-        foreach (Transform child in transform) {
-	    float i = 0;
-        i += 1;
-        Destroy(child.gameObject);
+        Instantiate(SelectionBox, piecePos, Quaternion.Euler(0, Rotation+90, 0), transform);
 
-		}
+
+    }
+    void OnMouseExit()
+    {
+
+        Destroy(transform.GetChild(0).gameObject);
+        // foreach (Transform dick in transform) {
+        // float i = 0;
+        // i += 1;
+        // Destroy(dick.gameObject);
+
     }
 }
+
