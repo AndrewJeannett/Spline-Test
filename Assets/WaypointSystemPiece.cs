18a4724dd1689fe30ac059966f1affe47b100366
AndrewJeannett <andrewjeannett@gmail.com>
2019-02-11 09:30:10 -0600

    update

--- Assets/WaypointSystemPiece.cs
+++ Assets/WaypointSystemPiece.cs
@@ -2,7 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaypointSystemPiece : MonoBehaviour {
+public class WaypointSystemPiece : MonoBehaviour
+{
     public List<Transform> Waypoints;
-   
+    private int ColCount=0;
+
+    void OnTriggerEnter(Collider col)
+    {
+        GameObject[] OtherWPs;
+        Debug.Log(ColCount);
+        OtherWPs = GameObject.FindGameObjectsWithTag("piece");
+
+        foreach (GameObject OtherWP in OtherWPs)
+        {
+            WaypointSystemPiece OtherComp = OtherWP.GetComponent<WaypointSystemPiece>();
+            if (col.gameObject == OtherWP)
+            {
+                ColCount++;
+                
+               
+                for (int i = 0; i <= OtherComp.Waypoints.Count; i++)
+                {
+                    if (OtherComp.ColCount >= 1)
+                    {
+                        Waypoints.Add(OtherComp.Waypoints[i]);
+                    }
+                }
+            }
+        }
+    }
 }
