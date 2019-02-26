a1c05ad53f4f648c76a025d13bf2acd6918d8077
AndrewJeannett <andrewjeannett@gmail.com>
2019-02-26 11:45:08 -0600

    fix 57.0

--- Assets/WaypointSystemPiece.cs
+++ Assets/WaypointSystemPiece.cs
@@ -7,88 +7,133 @@ public class WaypointSystemPiece : MonoBehaviour
     public List<Transform> Waypoints;
     public bool ColStart = false;
     GameObject[] OtherWPs;
-    BoxCollider ThisCol;
+    public BoxCollider ThisCol;
     public static int ColCount;
     private bool isColliding = false;
     private GameObject OtherWPc;
 
     private void Start()
-        
+
     {
-        
-        ThisCol = gameObject.GetComponent<BoxCollider>();
-        OtherWPs = GameObject.FindGameObjectsWithTag("piece");
+
+
+
+        ThisCol.enabled = true;
+        // StartCoroutine("ColliderDelay");
+
     }
+    //private void Update()
+    //{
+    //    OtherWPs = GameObject.FindGameObjectsWithTag("piece");
+    //}
+    //IEnumerator ColliderDelay()
+    //{
 
-    void OnTriggerEnter(Collider col)
+    //    yield return new WaitForSeconds(.5f);
+    //    ThisCol.enabled = true;
+    //}
+    void OnTriggerEnter(Collider other)
     {
-        Debug.Log(ColCount);
-        foreach (GameObject OtherWP in OtherWPs)
 
+        GameObject StartPiece = GameObject.FindGameObjectWithTag("StartPiece");
+        Collider StartCol = StartPiece.GetComponent<Collider>();
+        Debug.Log("SP=" + StartPiece);
+        Debug.Log(other.gameObject);
+        if (other.gameObject == StartPiece)
         {
-           
 
-
-            if ((col.gameObject == OtherWP) && (ColStart))
-
-            {
-                isColliding = true;
-                OtherWPc = OtherWP;
-            }
-            
-            ColCount++;
-
-           
-                
-            
+            Debug.Log("hit2");
+            gameObject.transform.SetParent(StartPiece.transform);
+            other.enabled = false;
+            gameObject.tag = "StartPiece";
+            ListAdd();
         }
-        
+        if ((other.gameObject.transform.parent == StartPiece.transform) && (transform.parent == null))
+        {
+
+            Debug.Log("hit2");
+
+            other.enabled = false;
+            gameObject.tag = "StartPiece";
+            gameObject.transform.SetParent(StartPiece.transform);
+            ListAdd();
+        }
     }
-    private void FixedUpdate()
-    {
-        Invoke("ListAdd", .1f);
-    }
-    void ColliderOff()
-    {
-        ThisCol.enabled = false;
-    }
+    //void OnTriggerEnter(Collider col)
+    //{
+
+    //    {
+    //        Debug.Log(ColCount);
+    //        foreach (GameObject OtherWP in OtherWPs)
+
+    //        {
+
+
+
+    //            if (col == OtherWP)
+
+    //            {
+    //                isColliding = true;
+
+    //               // OtherWP.transform.SetParent(transform.parent);
+    //                //OtherWP.tag = ("StartPiece");
+    //                ThisCol.enabled = false;
+    //            }
+
+    //            ColCount++;
+
+
+
+
+    //        }
+    //    }
+
+    //}
+
+    //private void FixedUpdate()
+
+
+    //    {
+    //    if (Input.GetButtonDown("Fire3"))
+    //    {
+    //        ThisCol.enabled = true;
+    //    }
+    //        Invoke("ListAdd", .1f);
+    //    }
+
+    //void ColliderOff()
+    //{
+    //    ThisCol.enabled = false;
+    //}
     void ListAdd()
     {
         //foreach (GameObject OtherWP in OtherWPs)
-        
-            //Transform[] Waypoints = GetComponentsInChildren<Transform>();
-            WaypointSystemPiece OtherClass = OtherWPc.GetComponent<WaypointSystemPiece>();
-            GameObject StartPiece = GameObject.FindGameObjectWithTag("StartPiece");
-            WaypointSystemPieceStart OtherStartClass = StartPiece.GetComponent<WaypointSystemPieceStart>();
-            if ((isColliding) && (ColStart))
-            {
-               
-            int LastIndex = OtherStartClass.Waypoints.Count - 1;
-                Transform LastIndexGO = OtherStartClass.Waypoints[LastIndex].GetComponent<Transform>();
-                float Dist = Vector3.Distance(LastIndexGO.position, transform.position);
-                Debug.Log(Dist);
-                if (Dist >= 5)
-                {
-                    Waypoints.Reverse();
-                }
-                for (int i = 0; i < Waypoints.Count; i++)
-                {
-                    if (Waypoints[i].transform.parent != StartPiece.transform)
-                    {
-                        OtherStartClass.Waypoints.Add(Waypoints[i]);
-                    Waypoints.Remove(Waypoints[i]);
-                    i--;
-                }
-               if (Waypoints.Count == 0)
-                {
-                  
 
-                    OtherClass.ColStart = true;
-                    OtherWPc.transform.SetParent(transform.parent);
-                    ColliderOff();
-                }
+        //Transform[] Waypoints = GetComponentsInChildren<Transform>();
+
+        GameObject StartPiece = GameObject.FindGameObjectWithTag("StartPiece");
+        WaypointSystemPieceStart OtherStartClass = StartPiece.GetComponent<WaypointSystemPieceStart>();
+
+
+        int LastIndex = OtherStartClass.Waypoints.Count - 1;
+        Transform LastIndexGO = OtherStartClass.Waypoints[LastIndex].GetComponent<Transform>();
+        float Dist = Vector3.Distance(LastIndexGO.position, transform.position);
+        Debug.Log(Dist);
+        if (Dist >= 3.5f)
+        {
+            Waypoints.Reverse();
+        }
+        for (int i = 0; i < Waypoints.Count; i++)
+        {
+            if (Waypoints[i].transform.parent != StartPiece.transform)
+            {
+                OtherStartClass.Waypoints.Add(Waypoints[i]);
+                Waypoints.Remove(Waypoints[i]);
+                i--;
             }
             
         }
-        }
     }
+}
+
+//}
--- Assets/WaypointSystemPieceStart.cs
+++ Assets/WaypointSystemPieceStart.cs
@@ -5,63 +5,85 @@ using UnityEngine;
 
 public class WaypointSystemPieceStart : MonoBehaviour
 {
+    public Rigidbody ThisRig;
     public List<GameObject>Pieces;
     public List<Transform> Waypoints;
     public GameObject player;
     private int current = 0;
     public int speed;
     public float WpRadius = .1f;
-    private bool ColTrue = true;
+    private bool ColTrue;
+    public BoxCollider ThisCol;
+    GameObject[] OtherWPs;
     // Use this for initialization
     void Start()
     {
-
+      
+        ColTrue = true;
     }
-    void OnTriggerStay(Collider col)
+  
+    //void OnTriggerEnter(Collider col)
+    //{
+    //    Debug.Log("hit");
+
+        
+    //    OtherWPs = GameObject.FindGameObjectsWithTag("piece");
+
+    //    foreach (GameObject OtherWP in OtherWPs)
+    //        {
+    //        ThisRig.WakeUp();
+    //        WaypointSystemPiece otherClass;
+    //        otherClass = OtherWP.GetComponent<WaypointSystemPiece>();
+    //        if (col.gameObject == OtherWP)
+    //        {
+               
+    //            otherClass.ColStart = true;
+                
+    //            //Pieces.Add(OtherWP);
+    //            // ColTrue = false;
+
+               
+    //            OtherWP.tag = ("StartPiece");
+                
+              
+    //            OtherWP.transform.SetParent(transform);
+    //            ThisCol.enabled = false;
+                
+               
+
+    //        }
+          
+
+    //        //else if (col.gameObject != OtherWP)
+    //        //{
+    //        //    ColTrue = false;
+    //        //}
+    //    }
+
+    //    }
+    
+
+
+
+
+
+
+
+
+    private void Update()
     {
-        if (ColTrue)
-        {
-            GameObject[] OtherWPs;
-            //Creates an array of all pieces in scene
-            OtherWPs = GameObject.FindGameObjectsWithTag("piece");
+       
             
-            
-            
-            foreach (GameObject OtherWP in OtherWPs)
-            {
+       
+        //OtherWPs = GameObject.FindGameObjectsWithTag("piece");
+        //Creates an array of all pieces in scene
 
-                if (col.gameObject == OtherWP)
-                {
-                    WaypointSystemPiece otherClass;
-                    otherClass = OtherWP.GetComponent<WaypointSystemPiece>();
-                    otherClass.ColStart = true;
-                    OtherWP.transform.SetParent(transform);
-                    //Pieces.Add(OtherWP);
-                    ColTrue = false;
-                    
-                }
-             
-                //else if (col.gameObject != OtherWP)
-                //{
-                //    ColTrue = false;
-                //}
-            }
-
-        }
-    }
-
-
-
-
-
-
-
-
-    void Update()
-    {
+       
         //  Debug.Log("count=" + Waypoints.Count);
-        // Debug.Log("current=" + current);
+         Debug.Log("current=" + current);
+
         player.transform.position = Vector3.MoveTowards(player.transform.position, Waypoints[current].transform.position, Time.deltaTime * speed);
+       
         if (Vector3.Distance(Waypoints[current].transform.position, player.transform.position) < WpRadius)
         {
             if (speed < 0)
