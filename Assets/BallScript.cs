0e5263418342df00d8fc64e8e20bb5b8377e9427
AndrewJeannett <andrewjeannett@gmail.com>
2019-03-03 14:32:17 -0600

    update 3.0

--- Assets/BallScript.cs
+++ Assets/BallScript.cs
@@ -11,27 +11,30 @@ public class BallScript : MonoBehaviour {
     public float WpRadius = .1f;
     public float RampSpeed;
     public float StraightSpeed;
-    private bool Forward;
+    private bool Play;
     // Use this for initialization
     void Start () {
         StartClass = Startpiece.GetComponent<WaypointSystemPieceStart>();
-        
+        Play = false;
 	}
     public void OnTriggerStay(Collider Col)
     {
-        if (Col.tag == "up")
+        if (Play)
         {
-            Debug.Log("slow");
-            speed-=1*RampSpeed;
-        }
-        if (Col.tag == "down")
-        {
-            Debug.Log("fast");
-            speed+=1*RampSpeed;
-        }
-        if (Col.tag == "straight")
-        {
-            speed -= 1*StraightSpeed;
+            if (Col.tag == "up")
+            {
+                Debug.Log("slow");
+                speed -= 1 * RampSpeed;
+            }
+            if (Col.tag == "down")
+            {
+                Debug.Log("fast");
+                speed += 1 * RampSpeed;
+            }
+            if (Col.tag == "straight")
+            {
+                speed -= 1 * StraightSpeed;
+            }
         }
     }
     // Update is called once per frame
@@ -46,35 +49,40 @@ public class BallScript : MonoBehaviour {
          foreach (GameObject down in DownObjects){
             down.tag = "up";
         }
-        Forward=true;
+        
     }
     private void Update()
     {
 
         Debug.Log("speed=" + speed);
        
-
-        transform.position = Vector3.MoveTowards(transform.position, StartClass.Waypoints[current].transform.position, Time.deltaTime * speed);
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (Play == false)
+            {
+                Play = true;
+            }else if (Play)
+            {
+                Play = false;
+            }
+        }
+        if (Play)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, StartClass.Waypoints[current].transform.position, Time.deltaTime * speed);
+        }
             if (speed <= 0)
             {
             SpeedSwitch();
                
                     speed++;
             }
-            else if (speed > .01)
-            {
-                Forward=true;
-            }
+          
         if (Vector3.Distance(StartClass.Waypoints[current].transform.position, transform.position) < WpRadius)
         {
-           if (Forward){
+        
                current++;
-           }
-           if (Forward== false){
-               
-             
-               
-           }
+           
+         
             if (current >= StartClass.Waypoints.Count)
             {
                 current = 0;
